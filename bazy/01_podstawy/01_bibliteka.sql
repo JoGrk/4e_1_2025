387a5e6ebc5e7338043181207acda7747cbfc0c8
Jolanta Gryko <jgryko@gmail.com>
2025-09-05 08:45:44 +0200

    5.09

--- bazy/01_podstawy/01_bibliteka.sql
+++ bazy/01_podstawy/01_bibliteka.sql
@@ -1,2 +1,29 @@
 -- start
 show databases;
+mysql -u root
+CREATE DATABASE 4e_1_biblioteka;
+SHOW DATABASES;
+USE 4e_1_biblioteka
+CREATE TABLE ksiazka(
+    tytul varchar(255),
+    autor varchar(255),
+    cena int
+);
+SHOW TABLES;
+DESC ksiazka;
+INSERT INTO ksiazka
+VALUES
+('Harry Potter','J.K. Rowling',25);
+SELECT * FROM ksiazka;
+
+
+INSERT INTO ksiazka
+VALUES
+('Lalka','B. Prus',45);
+
+DELETE FROM ksiazka
+WHERE tytul = 'Harry Potter';
+
+ALTER TABLE ksiazka
+ADD id int primary key
+AUTO_INCREMENT;
