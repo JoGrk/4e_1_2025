7bd21ff055c4c23e27e4d05a74fd2329a95c1e54
Jolanta Gryko <jgryko@gmail.com>
2025-10-22 08:21:45 +0200

    22.10

--- tizbd/02_normalizacja/01_pierwsza.sql
+++ tizbd/02_normalizacja/01_pierwsza.sql
@@ -59,6 +59,41 @@ INSERT INTO Pracownicy
 (id_pracownika, imie, nazwisko, jezyk, ulica, numer_domu, miejscowosc)
 VALUES
 (1, 'Jan', 'Kowalski', 'PHP', 'os. Wł. Łokietka', '3/4', 'Naklo nad Notecia'),
-(2, 'Antoni', 'Malinowski', 'HTML CSS PHP' ,'', '1','Chrzastowo'),
-(3, 'Jan', 'Malinowski', 'Java  HTML', 'ul. Ogrodowa', '2', 'Nowa Wies Wielka'),
-(4, 'Andrzej', 'Ziemianski', 'JavaScript CSS HTML','ul. Sowia', '5', 'Nowa Wies');
+(2, 'Antoni', 'Malinowski', 'HTML' ,'', '1','Chrzastowo'),
+(2, 'Antoni', 'Malinowski', 'CSS' ,'', '1','Chrzastowo'),
+(2, 'Antoni', 'Malinowski', 'PHP' ,'', '1','Chrzastowo'),
+(3, 'Jan', 'Malinowski', 'Java', 'ul. Ogrodowa', '2', 'Nowa Wies Wielka'),
+(3, 'Jan', 'Malinowski', 'HTML', 'ul. Ogrodowa', '2', 'Nowa Wies Wielka'),
+(4, 'Andrzej', 'Ziemianski', 'JavaScript','ul. Sowia', '5', 'Nowa Wies'),
+(4, 'Andrzej', 'Ziemianski', 'CSS','ul. Sowia', '5', 'Nowa Wies'),
+(4, 'Andrzej', 'Ziemianski', 'HTML','ul. Sowia', '5', 'Nowa Wies');
+
+
+-- B. Wypisz pracowników, którzy znają język Java
+SELECT * FROM Pracownicy
+WHERE jezyk='Java';
+
+-- D. Wypisz pracowników mieszkających w Nowej Wsi
+SELECT * FROM Pracownicy
+WHERE miejscowosc ='Nowa Wies';
+-- E. Wypisz wszystkie nazwy miejscowości pracowników
+SELECT DISTINCT miejscowosc 
+FROM Pracownicy
+ORDER BY miejscowosc;
+-- C. Wypisz imiona i nazwiska pracowników posortowane według nazwisk (ORDER BY)
+
+SELECT DISTINCT imie, nazwisko
+FROM Pracownicy
+ORDER BY nazwisko;
+
+-- 1. Każda porządna tabela powinna posiadać klucz, dodaj pole jeśli trzeba. 
+
+
+
+-- 2. Jan Malinowski poznał nowy język (CSS) - dopisz do tabeli.
+
+-- 3. Antoni Malinowski poznał nowy język - JavaScript i zmienił adres na Naklo nad Notecia, Staszica 1 - dopisz nowy wiersz do tabeli
+
+-- 4. Usuwamy informację o znajomości PHP (bo tak)
+
+-- 5. Popraw strukturę tabeli tak, aby nie było tych problemów (redundacja, anomalia wstawiania, aktualizacji, usuwania)
