ce0a17475d9a778d9da487273c62603929fd8f5a
Jolanta Gryko <jgryko@gmail.com>
2025-10-24 09:34:19 +0200

    24.10

---
+++ bazy/03_SELECT/03_napoje.sql
@@ -0,0 +1,37 @@
+-- A. Wyświetl napoje, których składnikiem głównym jest woda sodowa.
+ 
+ SELECT nazwa, skladnik_glowny
+ FROM proste_drinki
+ WHERE skladnik_glowny = 'woda sodowa';
+-- B. Wyświetl wszystkie napoje, których głównym składnikiem jest dowolny sok.
+ select nazwa, skladnik_glowny
+ from proste_drinki
+ where skladnik_glowny like 'sok%';
+-- C. Wyświetl wszystkie napoje, których głównym składnikiem jest dowolny sok lub nektar w ilości co najmniej 60 (ilosc1) ( ilość obowiązuje i przy soku, i przy nektarze)
+ 
+ SELECT nazwa, skladnik_glowny, ilosc1
+ FROM proste_drinki
+ WHERE (skladnik_glowny LIKE 'sok%' OR skladnik_glowny LIKE 'nektar%') 
+    AND ilosc1 >= 60;
+ 
+-- D. Wyświetl wszystkie napoje, których składnikiem dodatkowym jest dowolny sok w ilości 22 (ilosc2)
+
+SELECT nazwa, skladnik_dodatkowy, ilosc2
+ FROM proste_drinki
+ WHERE skladnik_dodatkowy LIKE 'sok%' 
+    AND ilosc2=22;
+ 
+-- E. Wyświetl wszystkie napoje, których nazwa zaczyna się od B i kończy na n
+ select nazwa 
+ FROM proste_drinki 
+ WHERE nazwa like 'B%n';
+-- F. Wyświetl wszystkie napoje, w których ilość dodatkowego składnika (ilosc2) jest wartością z zakresu od 30 do 180
+ 
+ SELECT nazwa, skladnik_dodatkowy, ilosc2
+ FROM proste_drinki
+ WHERE ilosc2 BETWEEN 30 AND 180;
+-- G. Wyświetl nazwy napojów, w których składnikiem głównym lub dodatkowym jest herbata.
+
+SELECT nazwa, skladnik_dodatkowy, skladnik_glowny
+FROM proste_drinki
+WHERE skladnik_dodatkowy like '%herbata%' or skladnik_glowny like '%herbata%';
