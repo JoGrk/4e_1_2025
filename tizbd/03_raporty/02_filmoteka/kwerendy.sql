a5a6d8efbff68f9e73ad99bc572c597ff255ea08
Jolanta Gryko <jgryko@gmail.com>
2025-11-26 13:22:24 +0100

    26.11

---
+++ tizbd/03_raporty/02_filmoteka/kwerendy.sql
@@ -0,0 +1,16 @@
+-- a) Zwrócenie tytułów filmów, dla których gatunek to SF,
+
+SELECT tytul 
+FROM filmy
+WHERE gatunek = 'SF';
+
+-- b) Zwrócenie wszystkich tytułów filmów wraz z nazwiskami reżyserów,
+
+SELECT tytul, nazwisko
+FROM filmy JOIN rezyserzy ON filmy.RezyserID = rezyserzy.IDRezyser;
+
+-- c) Zwrócenie filmów, dla których ocena w recenzji jest równa 4, 
+
+SELECT tytul, gatunek, tresc
+FROM filmy JOIN recenzje ON filmy.RecenzjaID = recenzje.idrecenzja
+WHERE ocena = '4';
