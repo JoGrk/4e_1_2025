8acdc4ba9f2917d1f8ce2cb7417cd6c57145fefc
Jolanta Gryko <jgryko@gmail.com>
2025-11-28 11:49:34 +0100

    18.11

--- bazy/05_JOIN/01_egzam.sql
+++ bazy/05_JOIN/01_egzam.sql
@@ -30,3 +30,47 @@ FROM kursy
 GROUP BY nazwa;
 
 -- − Zapytanie 5 : wybierające imiona i nazwiska uczestników z tabeli uczestnicy, daty z tabeli kursy_uczestnicy oraz nazwy kursów z tabeli kursy. Należy posłużyć się relacją.
+
+SELECT imie, nazwisko, data, Nazwa
+FROM kursy  
+    INNER JOIN kursy_uczestnicy ON kursy.kod = kursy_uczestnicy.kod_kursu
+    INNER JOIN uczestnicy ON uczestnicy.id_uczestnika = kursy_uczestnicy.id_uczestnika;
+
+
+
+--     ‒ Zapytanie 1: wybierające jedynie nazwę, cenę i opis wszystkich abonamentów
+
+SELECT nazwa,cena, opis 
+FROM abonamenty
+
+-- ‒ Zapytanie 2: liczące średnią cenę abonamentów. Cena jest zaokrąglona do dwóch miejsc po przecinku oraz nadano nazwę kolumny (alias) na „Srednio_abonament”
+
+SELECT ROUND(AVG(cena),2)
+FROM abonamenty;
+
+-- ‒ Zapytanie 3: wyświetlające cechę oraz id abonamentów w których występuje. Dane uporządkuj według pola cecha.
+
+SELECT abonamenty_id, cecha
+FROM cechy
+    INNER JOIN szczegolyabonamentu ON cechy.id = szczegolyabonamentu.abonamenty_id
+ORDER BY cecha;
+
+-- ‒ Zapytanie 4: wyświetlające cechę wraz z liczbą abonamentów, w których występuje. Dane pogrupuj według pola cecha. Należy zastosować relację.
+
+SELECT
+    cecha,
+    COUNT(*) AS wystpienia
+FROM cechy
+    INNER JOIN szczegolyabonamentu ON cechy.id = szczegolyabonamentu.cechy_id
+GROUP BY cecha;
+
+-- ‒ Zapytanie 5: wybierające jedynie nazwę abonamentu i odpowiadającą mu cechę dla abonamentu o id równym 1. Należy zastosować pełną relację n:m
+
+SELECT
+    nazwa,
+    cecha
+FROM abonamenty
+    INNER JOIN szczegolyabonamentu 
+    ON abonamenty.id = szczegolyabonamentu.abonamenty_id
+    INNER JOIN cechy ON cechy.id = szczegolyabonamentu.cechy_id
+WHERE abonamenty_id = 1;
