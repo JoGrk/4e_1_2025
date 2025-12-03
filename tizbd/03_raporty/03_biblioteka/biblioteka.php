684c5fa46a662c7a1c1c0ecdde2f4904dddcd5b0
Jolanta Gryko <jgryko@gmail.com>
2025-12-03 08:56:49 +0100

    3.12

--- tizbd/03_raporty/03_biblioteka/biblioteka.php
+++ tizbd/03_raporty/03_biblioteka/biblioteka.php
@@ -12,16 +12,43 @@
             WHERE nazwa = 'dramat';";
         $result = $link -> query($sql);
         $titles = $result -> fetch_all(1);
+         
+    $sql ="SELECT
+    tytul,data_wypozyczenia
+    FROM ksiazki
+    INNER JOIN wypozyczenia ON ksiazki.id = wypozyczenia.id_ksiazka
+    WHERE id_czytelnik = 2;";
+    $result = $link -> query($sql);
+    $hires = $result -> fetch_all(1);
     
+    $sql ="SELECT 
+                imie,
+                nazwisko,
+                COUNT(*) AS ile_tytulow
+            FROM autorzy
+                INNER JOIN ksiazki ON autorzy.id = ksiazki.id_autor
+            GROUP BY id_autor;";
+    $result = $link -> query($sql);
+    $autors = $result -> fetch_all(1);
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        table,td,th{
+            border:1px solid black;
+        }
+        table{
+            border-collapse:collapse;
+        }
+    </style>
 </head>
+
 <body>
     <h2>czytelnicy</h2>
     <!-- <p> [imie] <strong>[nazwisko]</strong></p> -->
@@ -47,6 +74,45 @@
         ?>
     </ul>
 
+    <ol> 
+        <!-- <li>['tytul'] <em>['data']</em></li> -->
+         <?php
+         foreach($hires as $hire){
+            echo "
+                <li>{$hire['tytul']} <em>{$hire['data_wypozyczenia']}</em></li>
+                ";
+
+         }
+
+         ?>
+    </ol>
+
+    <table>
+        <tr>
+            <th>Imię</th>
+            <th>Nazwisko</th>
+            <th>Ilość tytułów</th>
+        </tr>
+        <!-- <tr>
+            <td> ['imie']</td>
+            <td> ['nazwisko']</td>
+            <td> ['ile_tytulow']</td>
+        </tr> -->
+
+        <?php 
+            foreach($autors as $autor){
+                echo"
+                 <tr>
+                    <td> {$autor['imie']}</td>
+                    <td> {$autor['nazwisko']}</td>
+                    <td> {$autor['ile_tytulow']}</td>
+                </tr>
+                ";   
+            }
+        ?>
+    </table>
+
+    
 
 </body>
 </html>
