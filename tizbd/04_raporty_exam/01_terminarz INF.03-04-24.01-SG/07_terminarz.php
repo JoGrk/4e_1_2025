a473ea8e98f70c3f732515d0f1adb1aa78de4af9
Jolanta Gryko <jgryko@gmail.com>
2026-01-12 13:22:05 +0100

    12.01

--- tizbd/04_raporty_exam/01_terminarz INF.03-04-24.01-SG/07_terminarz.php
+++ tizbd/04_raporty_exam/01_terminarz INF.03-04-24.01-SG/07_terminarz.php
@@ -1,6 +1,23 @@
 <?php
     $link = new mysqli('localhost', 'root', '', '4e_1_terminarz');
 
+    $sql = "
+    SELECT DISTINCT
+        wpis
+    FROM zadania
+    WHERE dataZadania BETWEEN '2020-07-01' AND '2020-07-07' AND wpis IS NOT NULL AND wpis != '';";
+    $result = $link -> query($sql);
+    $tasks = $result -> fetch_all(1);
+
+    $sql = "SELECT
+                dataZadania,
+                wpis
+            FROM zadania
+            WHERE miesiac = 'lipiec';
+    ";
+    $result = $link -> query($sql);
+    $tasks2 = $result -> fetch_all(1);
+    
 ?>
 
 <!DOCTYPE html>
@@ -25,17 +42,31 @@
                 <?php
                 // ===== SKRYPT 1 =====
                 // tu powinien znaleźć się kod PHP wyświetlający najbliższe zadania
-                echo "brak danych";
+                foreach($tasks as $task){
+                    echo"{$task['wpis']}; ";
+                }
                 ?>
             </p>
         </section>
     </header>
 
     <main>
+
+        <!-- <section class="calendar">
+            <h6>[dataZadania]</h6>
+            <p>[wpis]</p>
+        </section> -->
         <?php
         // ===== SKRYPT 2 =====
         // tu powinien znaleźć się kod PHP generujący bloki kalendarza
-        echo "<div>kalendarz – brak danych</div>";
+        foreach($tasks2 as $task2){
+            echo"
+            <section class='calendar'>
+                <h6> {$task2['dataZadania']} </h6>
+                <p> {$task2['wpis']} </p>
+            </section>
+            ";
+        }
         ?>
     </main>
 
