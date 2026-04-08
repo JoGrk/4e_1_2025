b4b194a5a688dcda029b6ee450f657c89468a5ac
Jolanta Gryko <jgryko@gmail.com>
2026-04-08 08:09:27 +0200

    1.04

---
+++ tizbd/05_formularze/06_dania/index.php
@@ -0,0 +1,51 @@
+<?php
+
+    $link = new mysqli('localhost','root','','4e_1_baza');
+
+    $sql = "SELECT 
+                id,
+                nazwa
+            FROM typy_dan";
+    $result = $link -> query($sql);
+    $types = $result -> fetch_all(1);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <form action="dodawanie.php" method="post">
+
+        <label for="dish-name">Nazwa</label>
+        <input type="text" name="dish-name" id="dish-name"><br>
+
+        <label for="dish-type">Typ dania:</label>
+        <select name="dish-type" id="dish-type">
+            <!-- skrypt a -->
+             <!-- <option value='[id]'>[nazwa]</option> -->
+            <?php
+                foreach($types as $type)
+                    echo"<option value='{$type['id']}'>{$type['nazwa']}</option>";
+                
+            ?>
+        </select><br>
+
+        <label for="price">Cena</label>
+        <input type="number" name="price" id="price"><br>
+
+        <button>Wyślij</button>
+
+    </form>
+
+
+</body>
+</html>
+
+<?php
+$link -> close();
+?>
---
+++ tizbd/05_formularze/06_dania/kwerendy.sql
@@ -0,0 +1,24 @@
+-- 1. Dodaje nowe danie do tabeli dania (id typu, nazwa, cena) (4, 'Herbata', 3)
+
+INSERT INTO dania
+(typ, nazwa, cena)
+VALUES
+(4, 'Herbata', 3);
+
+-- 2. Wyświetla dostępne dania (wybierz wersję): 
+-- nazwy typów dań i nazwy dań wraz z ceną (tabela dania i typy_dan)
+
+SELECT dania.nazwa, cena, typy_dan.nazwa
+FROM dania  
+    INNER JOIN typy_dan ON dania.typ = typy_dan.id;
+-- lub niżej punktowane zapytanie wyświetlajace tylko id typu, nazwę i cenę z tabeli dania 
+
+
+
+
+-- 3. Wyświetla id i nazwę z tabeli typy_dan
+
+SELECT 
+    id,
+    nazwa
+FROM typy_dan;
